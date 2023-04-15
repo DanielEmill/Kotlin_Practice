f35af7a3b9a5938ac44e07598c8a3e247df18a74
Emill Daniel Garcia Castellanos <Danielemill2012@gmail.com>
2023-04-15 12:52:28 -0400

    Creacion del repositiorio + Codigo de practica

---
+++ Exercise.kt
@@ -0,0 +1,28 @@
+//Ejercicio 1 Println y print
+fun main()
+{
+    println("Tuesday")
+    println("Thursday")
+    println("Wednesday")
+    println("Friday")
+    println("Monday")
+    println("============================")
+    print("Hola Esto es una prueba ")
+    print("Sin Salto de linea")
+}
+//Ejercicio 2 Variables y concatenar
+fun main() {
+    val numero: Int = 2
+    val numero2: Int = 4
+    println("la suma de $numero y $numero2 es ${numero+numero2}")
+}
+fun main() {
+    val numero: Double = 2.4
+    val numero2: Double = 4.6
+    println("la multiplicacion de $numero y $numero2 es ${numero*numero2}")
+}
+// Omitiendo el tipo de la variable.
+fun main() {
+	val count = 2
+    println(count)
+}
