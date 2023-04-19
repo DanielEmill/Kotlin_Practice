b831806f939809a023c98d12991a5452db6a156c
Emill Daniel Garcia Castellanos <Danielemill2012@gmail.com>
2023-04-19 18:59:00 -0400

    Update from image and text.

--- FirstAppAndroid/app/src/main/java/com/example/firstapp/MainActivity.kt
+++ FirstAppAndroid/app/src/main/java/com/example/firstapp/MainActivity.kt
@@ -1,16 +1,30 @@
 package com.example.firstapp
-
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.firstapp.ui.theme.FirstAppTheme
+import androidx.compose.ui.unit.*
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.layout.ContentScale
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,29 +32,53 @@ class MainActivity : ComponentActivity() {
         setContent {
             FirstAppTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Emill")
+                Surface(color = MaterialTheme.colorScheme.background) {
+                    BirthdayGreetingWithImage(getString(R.string.happy_birthday_sam_text), "- from Emma")
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello, my name is $name!",
-        modifier = modifier
-    )
-}
 
-@Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun BirthdayGreetingWithText(message: String, from: String, modifier: Modifier = Modifier)
+{
+    Column(
+        modifier = modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Top,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = message,
+            fontSize = 36.sp,
+            modifier = Modifier
+                .padding(top = 16.dp)
+        )
+        Text(
+            text = from,
+            fontSize = 24.sp,
+            modifier = Modifier
+                .padding(top = 16.dp)
+        )
+    }
+}
+@Composable
+fun BirthdayGreetingWithImage(message: String, from: String, modifier: Modifier = Modifier) {
+    val image = painterResource(R.drawable.androidparty)
+    Box {
+        Image(
+            painter = image,
+            contentDescription = null,
+            contentScale = ContentScale.Crop
+        )
+        BirthdayGreetingWithText(message = message, from = from)
+    }
+}
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+fun BirthdayCardPreview() {
     FirstAppTheme {
-        Greeting("Emill")
+        BirthdayGreetingWithImage(message = "Happy Birthday Daniel!", from ="- from tu papa")
     }
 }
